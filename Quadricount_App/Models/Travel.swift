d6e22be2118b8796a32fae88c193c9ad1e8def35
Thomas Falcone <thomas.falcone06@gmail.com>
2019-03-25 14:39:20 +0100

    Add(Travel file)

---
+++ Quadricount_App/Models/Travel.swift
@@ -0,0 +1,13 @@
+//
+//  Travel.swift
+//  Quadricount_App
+//
+//  Created by Thomas Falcone on 25/03/2019.
+//  Copyright © 2019 Polytech Montpellier. All rights reserved.
+//
+
+import Foundation
+
+class Travel {
+
+}
